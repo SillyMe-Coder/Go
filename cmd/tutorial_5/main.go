57401b8b8c73d5700135b515d51f3c820b3f90e6
SillyMe-Coder <basraprithvipal@gmail.com>
2024-12-23 21:27:52 +0530

    first

--- cmd/tutorial_5/main.go
+++ cmd/tutorial_5/main.go
@@ -4,6 +4,23 @@ import "fmt"
 
 func main() {
 
-	var myString = "resume"
-	fmt.Println(myString)
+	var myString = []rune("résumé")
+	var indexed = myString[1]
+	fmt.Printf("%v, %T\n", indexed, indexed)
+	for i, v := range myString{
+		fmt.Println(i, v)
+	}
+	fmt.Printf("\nThe lenght of 'mystring is %v", len(myString))
+
+	var myRune = 'a'
+	fmt.Printf("\nmyRune = %v", myRune)
+
+	var strSlice = []string{"s", "u", "b", "s", "c", "r", "i", "b", "e", "r"}
+	var catStr = ""
+	for i := range strSlice {
+		catStr += strSlice[i]	
+	}
+	fmt.Printf("\n%v", catStr)
+
+
 }
