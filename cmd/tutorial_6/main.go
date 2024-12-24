89a49b5ed3d15fbbabae6776a32db9c721758851
SillyMe-Coder <basraprithvipal@gmail.com>
2024-12-24 19:28:01 +0530

    owner

--- cmd/tutorial_6/main.go
+++ cmd/tutorial_6/main.go
@@ -1,11 +1,20 @@
 package main
 
+import "fmt"
+
 type gasEngine struct {
-	npg uint8
+	mpg uint8
 	gallons uint8
+	owner
+}
+
+type owner struct {
+	name string
 }
 
 func main() {
 
-	var  nyEngine gasEngine
+	var  myEngine gasEngine = gasEngine{30, 10, owner{"John Doe"}}
+	myEngine.mpg = 35
+	fmt.Println(myEngine.mpg, myEngine.gallons, myEngine.name)
 }
