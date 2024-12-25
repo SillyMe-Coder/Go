33e47ad70bc6b085cbbcd777987768bffa77ae5e
SillyMe-Coder <basraprithvipal@gmail.com>
2024-12-25 15:27:07 +0530

    removed extra

--- cmd/tutorial_6/main.go
+++ cmd/tutorial_6/main.go
@@ -5,17 +5,12 @@ import "fmt"
 type gasEngine struct {
 	mpg uint8
 	gallons uint8
-	owner
-	int
-}
 
-type owner struct {
-	name string
 }
 
 func main() {
 
-	var  myEngine gasEngine = gasEngine{30, 10, owner{"John Doe"}, 10}
-	myEngine.mpg = 35
-	fmt.Println(myEngine.mpg, myEngine.gallons, myEngine.name)
+	var  myEngine gasEngine = gasEngine{30, 10}
+
+	fmt.Println(myEngine.mpg, myEngine.gallons)
 }
