0ef175bce22f1a210c1d6beaac3eaa27f431a410
SillyMe-Coder <basraprithvipal@gmail.com>
2024-12-25 15:29:29 +0530

    rm

--- cmd/tutorial_6/main.go
+++ cmd/tutorial_6/main.go
@@ -14,10 +14,7 @@ type gasEngine struct {
 func main() {
 
 	var  myEngine gasEngine = gasEngine{30, 10}
-	var myEngine2 = struct {
-		mpg uint8
-		gallons unit8
-	} {25, 15}// not reusable
+
 
 	fmt.Println(myEngine.mpg, myEngine.gallons)
 }
