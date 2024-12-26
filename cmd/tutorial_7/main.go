d48be906f4340adaba86034829c3c5ab78e42a17
SillyMe-Coder <basraprithvipal@gmail.com>
2024-12-26 20:23:58 +0530

    ointers functions and memory

--- cmd/tutorial_7/main.go
+++ cmd/tutorial_7/main.go
@@ -1,13 +1,19 @@
 package main
 
 import "fmt"
-//pointers slices
+//pointers functions and memory
 
 func main() {
-	var slice = []int32{1, 2, 3}
-	var sliceCopy = slice
+	var thing1 = [5]float64{1, 2, 3, 4, 5}
+	fmt.Printf("\nThe memory location of the thing1 array is: %p", &thing1)
+	var result [5]float64 = square(&thing1)
+	fmt.Printf("\nThe result is: %v", result)
+}
 
-	sliceCopy[2] = 4
-	fmt.Println(slice)
-	fmt.Println(sliceCopy)
+func square(thing2 *[5]float64) [5]float64{
+	fmt.Printf("\nThe memory location of the thing2 array is: %p", &thing2)
+	for i := range thing2 {
+		thing2[i] = thing2[i] * thing2[i]
+	}
+	return *thing2
 }
