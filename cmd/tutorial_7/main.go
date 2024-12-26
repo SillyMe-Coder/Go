8f0a79bce801210f7405fa4436518483f34a1c08
SillyMe-Coder <basraprithvipal@gmail.com>
2024-12-26 20:13:22 +0530

    slices debuging problem

--- cmd/tutorial_7/main.go
+++ cmd/tutorial_7/main.go
@@ -1,20 +1,13 @@
 package main
 
 import "fmt"
-//pointers
+//pointers slices
 
 func main() {
-	var p *int32 = new(int32)
-	var i int32
+	var slice = []int32{1, 2, 3}
+	var sliceCopy = slice
 
-	fmt.Printf("The value  p points to is: %v", *p)
-	fmt.Printf("\nThe value of i is: %v", i)
-	p = &i
-	*p = 1
-
-	fmt.Printf("\nThe value  p points to is: %v", *p)
-	fmt.Printf("\nThe value of i is: %v", i)
-
-	var k int32 = 2
-	i = k
+	sliceCopy[2] = 4
+	fmt.Println(slice)
+	fmt.Println(sliceCopy)
 }
