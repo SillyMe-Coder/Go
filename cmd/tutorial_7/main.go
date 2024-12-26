09268324a3402c3af54283efd04f77c7150abbd4
SillyMe-Coder <basraprithvipal@gmail.com>
2024-12-26 20:06:46 +0530

    pointers

---
+++ cmd/tutorial_7/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+//pointers
+
+func main() {
+	var p *int32 = new(int32)
+	var i int32
+
+	fmt.Printf("The value  p points to is: %v", *p)
+	fmt.Printf("\nThe value of i is: %v", i)
+	p = &i
+	*p = 1
+
+	fmt.Printf("\nThe value  p points to is: %v", *p)
+	fmt.Printf("\nThe value of i is: %v", i)
+}
